db2dc237309b13cc4db7d3651efe11d5c1bfdd84
gentechitsolutions <alexanderofficial567@gmail.com>
2024-11-06 02:38:57 -0500

    Update test.py

--- tests/test.py
+++ tests/test.py
@@ -1,11 +1,11 @@
 from src.math import add, sub
 
-def add_test():
+def test_add():
     assert add(2,3)==5
     assert add(1,5)==6
     assert add(-1,5)==4
 
-def sub_test():
+def test_sub():
     assert sub(2,3)==-1
     assert sub(4,3)==1
     assert sub(2,-3)==1
